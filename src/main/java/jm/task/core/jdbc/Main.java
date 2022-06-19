d96ca0e8664997b3b6aaaa51badeb7014cbf589c
GeorgyJavajun <tomatoesjuice@mail.ru>
2022-06-19 08:42:48 +0300

    Add logic in hte Dao; Add logic from Dao in UserService; Add Test in Main;

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,30 +1,23 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
 
-//        userDao.createUsersTable();
-//        userDao.saveUser("Ronnie", "Radke", (byte) 38);
-//        userDao.saveUser("Denny", "Worsnop", (byte) 31);
-//        userDao.saveUser("Serj", "Tankian", (byte) 45);
-//        userDao.saveUser("Mitch", "Lucker", (byte) 28);
-//        userDao.removeUserById(1);
-//        userDao.getAllUsers().forEach(System.out::println);
-//        userDao.cleanUsersTable();
-//        userDao.dropUsersTable();
+        userDao.createUsersTable();
+        userDao.saveUser("Ronnie", "Radke", (byte) 38);
+        userDao.saveUser("Denny", "Worsnop", (byte) 31);
+        userDao.saveUser("Serj", "Tankian", (byte) 45);
+        userDao.saveUser("Mitch", "Lucker", (byte) 28);
+        userDao.removeUserById(1);
+        userDao.getAllUsers().forEach(System.out::println);
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
 
 
 
     }
 }
-
-
-//USERSERVICEIMPL IMPLEMENTATION DAO!!!
