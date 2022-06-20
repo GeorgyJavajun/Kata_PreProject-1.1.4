ce1715213c473bb8bf4e64eeed9c51d12b25a6bb
GeorgyJavajun <tomatoesjuice@mail.ru>
2022-06-20 07:56:48 +0300

    Add folder "Resources" with properties for hibernate; Create SessionFactory;

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,13 @@
 package jm.task.core.jdbc.util;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.*;
 public class Util {
     private static final String USER = "root";
     private static final String PASSWORD = "PisunSQL278";
     private static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnect() {
         Connection connection = null;
@@ -16,5 +20,12 @@ public class Util {
             System.out.println("Connection fail");
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        sessionFactory = new Configuration()
+                .buildSessionFactory();
+
+        return sessionFactory;
     }
 }
