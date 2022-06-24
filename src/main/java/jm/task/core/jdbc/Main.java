446ed774ae2b6e9c9819ffc93b30bf20743e8290
GeorgyJavajun <103282860+GeorgyJavajun@users.noreply.github.com>
2022-06-24 16:25:45 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,20 +10,19 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.*;
 
-//1 2 3 4 5 6 7 8 9 10
 public class Main {
     public static void main(String[] args) {
             UserDao udh = new UserDaoHibernateImpl();
-//
-//        udh.createUsersTable();
-//        udh.saveUser("Ronnie", "Radke", (byte) 38);
-//        udh.saveUser("Denny", "Worsnop", (byte) 31);
-//        udh.saveUser("Serj", "Tankian", (byte) 45);
-//        udh.saveUser("Mitch", "Lucker", (byte) 28);
+
+        udh.createUsersTable();
+        udh.saveUser("Ronnie", "Radke", (byte) 38);
+        udh.saveUser("Denny", "Worsnop", (byte) 31);
+        udh.saveUser("Serj", "Tankian", (byte) 45);
+        udh.saveUser("Mitch", "Lucker", (byte) 28);
         udh.removeUserById(3);
         udh.getAllUsers().forEach(System.out::println);
-//        udh.cleanUsersTable();
-//        udh.dropUsersTable();
+        udh.cleanUsersTable();
+        udh.dropUsersTable();
 
     }
 }
